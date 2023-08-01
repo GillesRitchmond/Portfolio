d1d7e01df0096662a1fa993690ead3262092a856
GillesRitchmond <gritchmond@gmail.com>
2023-08-01 10:27:21 +0200

    Initialize

---
+++ src/app/portfolio.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link';
+
+export default function Portfolio() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <Link href="/page">
+          <a className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30" rel="noopener noreferrer">
+            <h2 className={`mb-3 text-2xl font-semibold`}>
+              Portfolio{' '}
+              <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                -&gt;
+              </span>
+            </h2>
+            <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+              Find in-depth information about Next.js features and API.
+            </p>
+          </a>
+        </Link>
+      </div>
+    </main>
+  )
+}
